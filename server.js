170a9c277ebd64f787dccd52534163eb77f6d2ad
Rebecca Jackson <nickandbecki@hotmail.co.uk>
2026-04-28 13:18:09 +0100

    Add signup page

--- server.js
+++ server.js
@@ -755,6 +755,35 @@ app.get('/login-page', (req, res) => {
   `);
 });
 
+app.get('/signup-page', (req, res) => {
+  res.send(`
+    <h2>Signup</h2>
+    <input id="email" placeholder="Email"><br><br>
+    <input id="password" type="password" placeholder="Password"><br><br>
+    <button onclick="signup()">Signup</button>
+
+    <script>
+      async function signup() {
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+
+        const res = await fetch('/signup', {
+          method: 'POST',
+          headers: {'Content-Type':'application/json'},
+          body: JSON.stringify({ email, password })
+        });
+
+        const text = await res.text();
+        alert(text);
+
+        if (res.ok) {
+          location.href = '/login-page';
+        }
+      }
+    </script>
+  `);
+});
+
 app.post('/add-business', async (req, res) => {
   const { name, twilioNumber, autoReplyMessage, ownerMobile } = req.body;
 
